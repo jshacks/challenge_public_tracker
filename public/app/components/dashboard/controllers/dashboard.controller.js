413e6b813ac20d7f54085d40f940b70d7df42782
Vyzor <murzac.vladimir@gmail.com>
2016-10-23 02:18:29 +0300

    bower json update

--- public/app/components/dashboard/controllers/dashboard.controller.js
+++ public/app/components/dashboard/controllers/dashboard.controller.js
@@ -40,7 +40,31 @@
             console.log('markers', map.markers);
             console.log('shapes', map.shapes);
             self.map = map;
+
+            var coord1 = new google.maps.LatLng(47, 29);
+            var coord2 = new google.maps.LatLng(46, 28);
+            var bounds = new google.maps.LatLngBounds();
+            bounds.extend(coord1);
+            bounds.extend(coord2);
+            map.fitBounds(bounds);
+
+            var pLineOpt = {
+                path: [coord1, coord2],
+                geodesic: true,
+                strokeColor: '#FF0000',
+                strokeOpacity: 0.7,
+                strokeWeight: 2
+            };
+
+            var pLine = new google.maps.Polyline(pLineOpt);
+
+            pLine.setMap(map);
+
         });
+
+        self.buildCurve = function(x, y) {
+
+        };
 
         self.showDetail = function(e, point) {
             self.point = point;
@@ -50,53 +74,6 @@
         // self.hideDetail = function() {
         //     self.map.hideInfoWindow('marker-info');
         // };
-
-        // NgMap.getMap({id: 'dashboardmap'}).then(function (map) {
-        //     for (var i = 0; i < self.firedAlerts.length; i++) {
-        //         if (self.currentfiredAlerts == self.firedAlerts[i].firedAlertId) {
-        //             var selectedfiredAlerts = self.firedAlerts[i];
-        //             bounds.extend(new google.maps.LatLng(selectedfiredAlerts.Thing.Latitude, selectedfiredAlerts.Thing.Longitude));
-        //
-        //             if (selectedfiredAlerts.Alert.MetricType == 'Geo_Breach' && selectedfiredAlerts.Thing.GeoDistance && selectedfiredAlerts.Thing.GeoDistance > 0) {
-        //                 var thiefIcon = '/assets/images/map/thief.png';
-        //                 //var radiusVal = distanceBetweenPoints_(new google.maps.LatLng(selectedfiredAlerts.Thing.Latitude, selectedfiredAlerts.Thing.Longitude),
-        //                 //    new google.maps.LatLng(selectedfiredAlerts.Thing.Longitude, selectedfiredAlerts.Thing.Curr_Longitude));
-        //
-        //                 var thiefCircle = new google.maps.Circle({
-        //                     strokeColor: '#FF0000',
-        //                     strokeOpacity: 0.8,
-        //                     strokeWeight: 2,
-        //                     fillColor: '#FF0000',
-        //                     fillOpacity: 0.15,
-        //                     map: map,
-        //                     center: new google.maps.LatLng(selectedfiredAlerts.Thing.Latitude, selectedfiredAlerts.Thing.Longitude),
-        //                     radius: selectedfiredAlerts.Alert.MetricMoreThanValue
-        //                     //selectedfiredAlerts.Thing.GeoDistance
-        //                 });
-        //
-        //                 var thiefMarker = new google.maps.Marker({
-        //                     position: {
-        //                         lat: selectedfiredAlerts.Thing.Curr_Latitude,
-        //                         lng: selectedfiredAlerts.Thing.Curr_Longitude
-        //                     },
-        //                     map: map,
-        //                     icon: thiefIcon,
-        //                     title: (selectedfiredAlerts.firedAlertsName ? selectedfiredAlerts.firedAlertsName : '')
-        //                 });
-        //
-        //                 bounds.extend(new google.maps.LatLng(selectedfiredAlerts.Thing.Curr_Latitude, selectedfiredAlerts.Thing.Curr_Longitude));
-        //                 map.fitBounds(bounds);
-        //                 map.setZoom(map.getZoom() - 1);
-        //             } else {
-        //                 map.fitBounds(bounds);
-        //                 map.setZoom(15);
-        //             }
-        //
-        //             return;
-        //
-        //         }
-        //     }
-        // });
 
         console.log('DashboardCtrl');
     }
