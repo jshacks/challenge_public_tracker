4e1cd66a58ef03db00e283f91435ba281f638550
Denis Povarov <povarov.denis@gmail.com>
2016-10-23 09:26:16 +0300

    working on mapping data

--- server/app/modules/request-parser.js
+++ server/app/modules/request-parser.js
@@ -9,14 +9,28 @@ var data = '';
 
 fs.createReadStream(file)
     .on('data', function (row) {
-        console.log(row + '');
         data += row + '';
     })
     .on('end', function () {
         console.log('data length: ', data.length);
         var array = data.split('\n');
         console.log('data length: ', array.length);
-        var jsonArray = JSON.stringify(array, false, 4);
+        var agencies = [];
+        var cleanArray = [];
+
+        // agencies = _.map(array, function (element) {
+        //     return element.releases[0].buyer;
+        // });
+
+        for (var i = 0; i < 100; i++) {
+            var obj = JSON.parse(array[i]);
+
+            if (obj.releases) {
+                cleanArray.push(obj.releases[0]);
+            }
+        }
+
+        console.log('cleanArray: ', cleanArray.length);
 
         // fs.writeFile("/Users/alexlipov/Projects/allData.json", jsonArray, function(err) {
         //     if(err) {
